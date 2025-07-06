df1e3816d2fcf99d6ddd19093fc3978bd2ee532c
caochihai <163825959+caochihai@users.noreply.github.com>
2025-07-06 14:20:36 +0700

    Update api.py

--- api.py
+++ api.py
@@ -145,7 +145,7 @@ async def summarize_pdf(request: PDFRequest = Body(...)):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(
-        app,
+        "api:app",
         host="0.0.0.0",
         port=5000,
         log_level="info",
